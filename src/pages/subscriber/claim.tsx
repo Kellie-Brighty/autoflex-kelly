9720b309a514be2063fbf5abf69f4bc4bad5eb0c
Parshva Runwal <runwalparshva3@gmail.com>
2023-07-22 15:00:26 +0530

    completing the subscriber claim page

---
+++ src/pages/subscriber/claim.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SubscriberClaimComponent from "@/components/subscriber/claim";
+
+export default function SubscriberClaim() {
+  return <SubscriberClaimComponent />;
+}
